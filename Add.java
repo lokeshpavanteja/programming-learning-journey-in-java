bd0ac1db2b07262c015011296b2dbb1c157d5e4c
lokeshpavanteja <lokeshpavantejakamparapu@gmail.com>
2026-03-08 11:27:18 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    public static void main(String[] args) {
+        int num = 5;
+        int num2 = 10;
+        int result = num + num2;
+        System.out.println(result);
+    }
+
+}
