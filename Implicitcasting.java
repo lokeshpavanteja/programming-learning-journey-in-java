735948effc8c02143f5466f911bc5448e2246e44
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-03-11 21:33:19 +0530

    Add files via upload

---
+++ Implicitcasting.java
@@ -0,0 +1,8 @@
+public class Implicitcasting {
+    public static void main(String[] args){
+        byte b = 127;
+        int a = b;
+        System.out.println(b);
+        System.out.println(a);
+    }
+}
