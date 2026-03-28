da0354d61cb64063f07d56aae0f84c0ae4bf935b
Kamparapu Lokesh Pavan Teja <lokeshpavantejakamparapu@gmail.com>
2026-03-28 22:30:35 +0530

    Pattern_8.java

--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,8 @@
+// Right Triangle Star
+
 public class Patterns {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 System.out.print(" * ");}
@@ -8,3 +10,14 @@ public class Patterns {
         }
     }
 }
+
+/*
+output:
+
+*
+**
+***
+****
+*****
+
+*/
